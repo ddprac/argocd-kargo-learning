e2bd0e1cc79ccc2b3617f14de4d7bb3c28b23114
ddprac33 <ddprac33@outlook.com>
2026-09-12 11:20:14 +0530

    chore:adding configmap & deployment

--- app/src/app.py
+++ app/src/app.py
@@ -1,5 +1,6 @@
 from doctest import debug
 from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
 @app.route("/")
@@ -10,6 +11,12 @@ def home():
 
 def health():
     return jsonify(status="healthy")
+
+@app.route("/config")
+def config():
+    return jsonify(
+        environment=os.getenv("APP_ENV","local")
+    )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=9000,debug=True)
